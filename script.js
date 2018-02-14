4e6752145d66153d53ee03b6181c1df5ba58d663
marleigh <volcana@gmail.com>
2018-02-13 19:38:57 -0800

    Mostly working, one small bug

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 var d = new Date();
-var list = document.querySelectorAll("li");
-//use querySelectorAll to get every instance of li!
-
+var test = document.getElementById("test");
+var textInput;
 
 //display today's date at the top
 document.getElementById("date").innerHTML = d.toDateString();
+//can this be changed to a better looking format? D:
 
+var items;
+//use querySelectorAll to get every instance of li!
+
+//create button variable and event listener to retrieve data
+var button = document.getElementById("submit");
+button.addEventListener("click", function(textInput) {
+  textInput = document.getElementById("addToList").value;
+
+  var newListItem = document.createElement("li");
+  var newListNode = document.createTextNode(textInput);
+
+  newListItem.appendChild(newListNode);
+
+  var list = document.getElementById("list");
+  // Insert <li> before the first child of <ul>
+  list.insertBefore(newListItem, list.childNodes[0]);
+  //run this to force it to loop through all List
+  //there's gotta be a better way?
+  updateLIs();
+
+
+});
 
 //loop through all LIs to determine cross out, or hover
-for(var i = 0; i < list.length; i++){
-  list[i].addEventListener("mouseover", function() {
-    this.classList.add("hover");
-  });
+function updateLIs() {
 
-  list[i].addEventListener("mouseout",function() {
-    this.classList.remove("hover");
-  });
+  items = document.querySelectorAll("li");
+  console.log(items);
 
-//also want to make a way to delete item once crossed off
-  list[i].addEventListener("click", function() {
-    this.classList.toggle("done");
-  });
+  for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("mouseover", function() {
+      this.classList.add("hover");
+    });
 
+    items[i].addEventListener("mouseout", function() {
+      this.classList.remove("hover");
+    });
+
+    //also want to make a way to delete item once crossed off
+    items[i].addEventListener("click", function() {
+      this.classList.toggle("done");
+      console.log("I am clicking???"); //BUG getting two clicks per click on the previous entry
+    });
+
+  }
 }
 
-var test = document.getElementById("test");
-
-//create button variable and
-var button = document.getElementById("submit");
-button.addEventListener("click", function() {
-  var textInput = document.getElementById("addToList").value;
-  console.log(textInput);
-});
-//need a way to add new items to List
-
-//save input to variable
-
-//add contents of input to a new li
-
-
+//TODO
 //delete items after they've been crossed out
-//
+//fix issue with the updateLIs function
